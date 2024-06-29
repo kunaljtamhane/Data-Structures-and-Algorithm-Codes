c146bde7164f2b70ff6e88bc7000f15be6e836c9
Kunal Jatin Tamhane <kunaljtamhane@gmail.com>
2024-06-29 00:13:14 -0500

    Create Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays{
+  public static void main (String[] args){
+    int[] arr = new int[5];
+    arr[0] = 23;
+    arr[1] = 45;
+    arr[2] = 233;
+    arr[3] = 543;
+    arr[4] = 3;
+    System.out.println(Arrays.toString(arr));
+    System.out.println(arr[3]);
+  }
+}
