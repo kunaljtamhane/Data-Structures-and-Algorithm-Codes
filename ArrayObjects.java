70a11a5b789a5ca8646bbd16c0925ef77808de81
Kunal Jatin Tamhane <kunaljtamhane@gmail.com>
2024-06-29 00:24:40 -0500

    Create ArrayObjects.java

---
+++ ArrayObjects.java
@@ -0,0 +1,9 @@
+public class ArrayObjects{
+  public static void main(String[]args){
+    String[] str = new String[4];
+    for(int i =0; i<str.length; i++){
+      str[i[ = in.next();
+    }
+    System.out.println(Arrays.toString(str));
+  }
+}
