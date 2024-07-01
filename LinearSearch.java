1c5b6b7cfd13970364d6deb131155cb3e9446d6f
Kunal Jatin Tamhane <kunaljtamhane@gmail.com>
2024-06-30 23:59:53 -0500

    Create LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,8 @@
+public class LinearSearch{
+  public static void main(String[]args){
+    int[]arr ={23,45,1,2,8,9,-3,16};
+    int target =19;
+    int ans = linearSearch(arr,target);
+    System.out.println(ans);
+  }
+}
