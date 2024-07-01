22f7a9ddf383a308a6be209caa7aac614a44676b
Kunal Jatin Tamhane <kunaljtamhane@gmail.com>
2024-07-01 00:02:49 -0500

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -5,4 +5,16 @@ public class LinearSearch{
     int ans = linearSearch(arr,target);
     System.out.println(ans);
   }
+  public static int linearSearch(int[]arr, int target){
+    if(arr.length==0){
+      return -1;
+    }
+    for(int i=0; i<arr.length;i++){
+      int element=arr[i];
+      if(element==target){
+        return i;
+      }
+    }
+    return -1;
+  }
 }
